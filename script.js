fb6235a49bd8d5d11211da4a4c4c33aab07ea621
navadeepak <128801710+navadeepak@users.noreply.github.com>
2024-01-31 12:57:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,261 @@
+function lamb(){
+    document.getElementById("carname").innerHTML=("Lamborghini")
+    document.getElementById("Lamborghini-but").style=("background-color:tomato")
+    document.getElementById("Bugatti-but").style=("background-color:royalblue")
+    document.getElementById("Ferrari-but").style=("background-color:royalblue")
+    document.getElementById("Mini-but").style=("background-color:royalblue")
+    document.getElementById("Porsche-but").style=("background-color:royalblue")
+    document.getElementById("Aston-but").style=("background-color:royalblue")
+    document.getElementById("Mercedes-but").style=("background-color:royalblue")
+    document.getElementById("McLaren-but").style=("background-color:royalblue")
+    document.getElementById("BMW-but").style=("background-color:royalblue")
+    document.getElementById("audi-but").style=("background-color:royalblue")
+    document.getElementById("carlogo").src="lamborghini-logo.png"
+    document.getElementById("carimg").src="lambi.jpg"
+    document.getElementById("carimg").style=("width:50vw;height:80vh")
+    document.getElementById("carlogo").style=("width:12.5vw;height:25vh;")
+    document.getElementById("alternamecompany").innerHTML=("Automobili Lamborghini S.p.A.")
+    document.getElementById("alterfoundername").innerHTML=("Ferruccio Lamborghini")
+    document.getElementById("alterfounded").innerHTML=("1963; 61 years ago")
+    document.getElementById("alterhead").innerHTML=("Sant'Agata Bolognese, Emilia-Romagna, Italy")
+    document.getElementById("alterproduct").innerHTML=("Increase 9,233 vehicles")
+    document.getElementById("alterowner").innerHTML=("Volkswagen Group")
+    document.getElementById("link").href="https://www.lamborghini.com/en-en"
+
+}
+function bug(){
+    document.getElementById("carname").innerHTML=("Bugatti")
+    document.getElementById("Lamborghini-but").style=("background-color:royalblue")
+    document.getElementById("Bugatti-but").style=("background-color:tomato")
+    document.getElementById("Ferrari-but").style=("background-color:royalblue")
+    document.getElementById("Mini-but").style=("background-color:royalblue")
+    document.getElementById("Porsche-but").style=("background-color:royalblue")
+    document.getElementById("Aston-but").style=("background-color:royalblue")
+    document.getElementById("Mercedes-but").style=("background-color:royalblue")
+    document.getElementById("McLaren-but").style=("background-color:royalblue")
+    document.getElementById("BMW-but").style=("background-color:royalblue")
+    document.getElementById("audi-but").style=("background-color:royalblue")
+    document.getElementById("carlogo").src="bugatti-logo-removebg-preview.png"
+    document.getElementById("carimg").src="bugatti.jpg"
+    document.getElementById("carimg").style=("width:65vw;height:55vh")
+    document.getElementById("carlogo").style=("width:20vw;height:30vh;")
+    document.getElementById("alternamecompany").innerHTML=("Bugatti Automobiles")
+    document.getElementById("alterfoundername").innerHTML=("Ettore Bugatti")
+    document.getElementById("alterfounded").innerHTML=("December 22, 1998; 25 years ago")
+    document.getElementById("alterhead").innerHTML=("Molsheim, France")
+    document.getElementById("alterproduct").innerHTML=("76 vehicles")
+    document.getElementById("alterowner").innerHTML=("Rimac Automobili")
+    document.getElementById("link").href="https://www.bugatti.com/"
+}
+function ferr(){
+    document.getElementById("carname").innerHTML=("Ferrari")
+    document.getElementById("Lamborghini-but").style=("background-color:royalblue")
+    document.getElementById("Bugatti-but").style=("background-color:royalblue")
+    document.getElementById("Ferrari-but").style=("background-color:tomato")
+    document.getElementById("Mini-but").style=("background-color:royalblue")
+    document.getElementById("Porsche-but").style=("background-color:royalblue")
+    document.getElementById("Aston-but").style=("background-color:royalblue")
+    document.getElementById("Mercedes-but").style=("background-color:royalblue")
+    document.getElementById("McLaren-but").style=("background-color:royalblue")
+    document.getElementById("BMW-but").style=("background-color:royalblue")
+    document.getElementById("audi-but").style=("background-color:royalblue")
+    document.getElementById("carlogo").src="ferrari-logo-removebg-preview.png"
+    document.getElementById("carimg").src="ferrari.jpg"
+    document.getElementById("carimg").style=("width:50vw;height:55vh")
+    document.getElementById("carlogo").style=("width:15vw;height:20vh;")
+    document.getElementById("alternamecompany").innerHTML=("Ferrari")
+    document.getElementById("alterfoundername").innerHTML=("Enzo Ferrari")
+    document.getElementById("alterfounded").innerHTML=("13 September 1939; 84 years ago in Modena, Italy")
+    document.getElementById("alterhead").innerHTML=("Maranello, Emilia-Romagna, Italy Modena, Emilia-Romagna, Italy")
+    document.getElementById("alterproduct").innerHTML=("13,221 units shipped")
+    document.getElementById("alterowner").innerHTML=("Exor N.V,Piero Ferrari")
+    document.getElementById("link").href="https://www.ferrari.com/en-IN"
+
+}
+function mini(){
+    document.getElementById("carname").innerHTML=("Mini Cooper")
+    document.getElementById("Lamborghini-but").style=("background-color:royalblue")
+    document.getElementById("Bugatti-but").style=("background-color:royalblue")
+    document.getElementById("Ferrari-but").style=("background-color:royalblue")
+    document.getElementById("Mini-but").style=("background-color:tomato")
+    document.getElementById("Porsche-but").style=("background-color:royalblue")
+    document.getElementById("Aston-but").style=("background-color:royalblue")
+    document.getElementById("Mercedes-but").style=("background-color:royalblue")
+    document.getElementById("McLaren-but").style=("background-color:royalblue")
+    document.getElementById("BMW-but").style=("background-color:royalblue")
+    document.getElementById("audi-but").style=("background-color:royalblue")
+    document.getElementById("carlogo").src="minicooper-logo-removebg-preview.png"
+    document.getElementById("carimg").src="Mini_Cooper.jpg"
+    document.getElementById("carimg").style=("width:60vw;height:65vh")
+    document.getElementById("carlogo").style=("width:20vw;height:20vh;")
+    document.getElementById("alternamecompany").innerHTML=("Mini Cooper")
+    document.getElementById("alterfoundername").innerHTML=(" Charles Cooper and his son John.")
+    document.getElementById("alterfounded").innerHTML=("Mini was founded in 1969")
+    document.getElementById("alterhead").innerHTML=("Munich, Germany")
+    document.getElementById("alterproduct").innerHTML=("3 products")
+    document.getElementById("alterowner").innerHTML=("BMW")
+    document.getElementById("link").href="https://www.mini.com/en_MS/home.html"
+
+}
+function porsch(){
+    document.getElementById("carname").innerHTML=("Porsche")
+    document.getElementById("Lamborghini-but").style=("background-color:royalblue")
+    document.getElementById("Bugatti-but").style=("background-color:royalblue")
+    document.getElementById("Ferrari-but").style=("background-color:royalblue")
+    document.getElementById("Mini-but").style=("background-color:royalblue")
+    document.getElementById("Porsche-but").style=("background-color:tomato")
+    document.getElementById("Aston-but").style=("background-color:royalblue")
+    document.getElementById("Mercedes-but").style=("background-color:royalblue")
+    document.getElementById("McLaren-but").style=("background-color:royalblue")
+    document.getElementById("BMW-but").style=("background-color:royalblue")
+    document.getElementById("audi-but").style=("background-color:royalblue")
+    document.getElementById("carlogo").src="porsche-logo-removebg-preview.png"
+    document.getElementById("carimg").src="porsche.webp"
+    document.getElementById("carimg").style=("width:70vw;height:55vh")
+    document.getElementById("carlogo").style=("width:20vw;height:20vh;")
+    document.getElementById("alternamecompany").innerHTML=("Porsche")
+    document.getElementById("alterfoundername").innerHTML=("Ferdinand Porsche")
+    document.getElementById("alterfounded").innerHTML=("1931; 93 years ago in Stuttgart, Germany")
+    document.getElementById("alterhead").innerHTML=("Stuttgart, Germany")
+    document.getElementById("alterproduct").innerHTML=("321,321 vehicles")
+    document.getElementById("alterowner").innerHTML=("Volkswagen AG , Porsche SE , QIA")
+    document.getElementById("link").href="https://www.porsche.com/international/"
+
+}
+function asto(){
+    document.getElementById("carname").innerHTML=("Aston Martin")
+    document.getElementById("Lamborghini-but").style=("background-color:royalblue;")
+    document.getElementById("Bugatti-but").style=("background-color:royalblue")
+    document.getElementById("Ferrari-but").style=("background-color:royalblue")
+    document.getElementById("Mini-but").style=("background-color:royalblue")
+    document.getElementById("Porsche-but").style=("background-color:royalblue")
+    document.getElementById("Aston-but").style=("background-color:tomato")
+    document.getElementById("Mercedes-but").style=("background-color:royalblue")
+    document.getElementById("McLaren-but").style=("background-color:royalblue")
+    document.getElementById("BMW-but").style=("background-color:royalblue")
+    document.getElementById("audi-but").style=("background-color:royalblue")
+    document.getElementById("carlogo").src="am-logo-removebg-preview.png"
+    document.getElementById("carimg").src="Aston Martin.png"
+    document.getElementById("carimg").style=("width:65vw;height:55vh")
+    document.getElementById("carlogo").style=("width:20vw;height:20vh;")
+    document.getElementById("alternamecompany").innerHTML=("Aston Martin")
+    document.getElementById("alterfoundername").innerHTML=("Lionel Martin,Robert Bamford")
+    document.getElementById("alterfounded").innerHTML=("15 January 1913; 111 years ago")
+    document.getElementById("alterhead").innerHTML=("Gaydon, Warwickshire, England, United Kingdom")
+    document.getElementById("alterproduct").innerHTML=("4 car models")
+    document.getElementById("alterowner").innerHTML=("Lawrence Sheldon Strulović")
+    document.getElementById("link").href="https://www.astonmartin.com/en/"
+
+}
+function benz(){
+    document.getElementById("carname").innerHTML=("Mercedes")
+    document.getElementById("Lamborghini-but").style=("background-color:royalblue")
+    document.getElementById("Bugatti-but").style=("background-color:royalblue")
+    document.getElementById("Ferrari-but").style=("background-color:royalblue")
+    document.getElementById("Mini-but").style=("background-color:royalblue")
+    document.getElementById("Porsche-but").style=("background-color:royalblue")
+    document.getElementById("Aston-but").style=("background-color:royalblue")
+    document.getElementById("Mercedes-but").style=("background-color:tomato")
+    document.getElementById("McLaren-but").style=("background-color:royalblue")
+    document.getElementById("BMW-but").style=("background-color:royalblue")
+    document.getElementById("audi-but").style=("background-color:royalblue")
+    document.getElementById("carlogo").src="benz-logo-removebg-preview.png"
+    document.getElementById("carimg").src="benz.jpg"
+    document.getElementById("carimg").style=("width:250vw;height:55vh;")
+    document.getElementById("carlogo").style=("width:15vw;height:25vh;")
+    document.getElementById("alternamecompany").innerHTML=("Mercedes-Benz")
+    document.getElementById("alterfoundername").innerHTML=("Karl Benz, Gottlieb Daimler, Wilhelm Maybach and Emil Jellinek")
+    document.getElementById("alterfounded").innerHTML=("28 June 1926; 97 years ago")
+    document.getElementById("alterhead").innerHTML=("Stuttgart, Germany")
+    document.getElementById("alterproduct").innerHTML=("2,164,187 cars worldwide")
+    document.getElementById("alterowner").innerHTML=("Mercedes-Benz AG")
+    document.getElementById("link").href="https://group.mercedes-benz.com/en/"
+
+}
+function mcl(){
+    document.getElementById("carname").innerHTML=("McLaren")
+    document.getElementById("Lamborghini-but").style=("background-color:royalblue")
+    document.getElementById("Bugatti-but").style=("background-color:royalblue")
+    document.getElementById("Ferrari-but").style=("background-color:royalblue")
+    document.getElementById("Mini-but").style=("background-color:royalblue")
+    document.getElementById("Porsche-but").style=("background-color:royalblue")
+    document.getElementById("Aston-but").style=("background-color:royalblue")
+    document.getElementById("Mercedes-but").style=("background-color:royalblue")
+    document.getElementById("McLaren-but").style=("background-color:tomato")
+    document.getElementById("BMW-but").style=("background-color:royalblue")
+    document.getElementById("audi-but").style=("background-color:royalblue")
+    document.getElementById("carlogo").src="mcl.png"
+    document.getElementById("carimg").src="McLarenArtura-7_crop-4x3.webp"
+    document.getElementById("carimg").style=("width:50vw;height:75vh;")
+    document.getElementById("carlogo").style=("width:20vw;height:20vh;")
+    document.getElementById("alternamecompany").innerHTML=("McLaren Automotive")
+    document.getElementById("alterfoundername").innerHTML=("Ron Dennis")
+    document.getElementById("alterfounded").innerHTML=("2 December 1985; 38 years ago")
+    document.getElementById("alterhead").innerHTML=("McLaren Technology Centre, Woking, England, UK")
+    document.getElementById("alterproduct").innerHTML=("2,188 units")
+    document.getElementById("alterowner").innerHTML=("The Bahrain Mumtalakat Holding Company")
+    document.getElementById("link").href="https://cars.mclaren.com/"
+
+}
+function bmw(){
+    document.getElementById("carname").innerHTML=("BMW")
+    document.getElementById("Lamborghini-but").style=("background-color:royalblue")
+    document.getElementById("Bugatti-but").style=("background-color:royalblue")
+    document.getElementById("Ferrari-but").style=("background-color:royalblue")
+    document.getElementById("Mini-but").style=("background-color:royalblue")
+    document.getElementById("Porsche-but").style=("background-color:royalblue")
+    document.getElementById("Aston-but").style=("background-color:royalblue")
+    document.getElementById("Mercedes-but").style=("background-color:royalblue")
+    document.getElementById("McLaren-but").style=("background-color:royalblue")
+    document.getElementById("BMW-but").style=("background-color:tomato")
+    document.getElementById("audi-but").style=("background-color:royalblue")
+    document.getElementById("carlogo").src="bmw-removebg-preview.png"
+    document.getElementById("carimg").src="bmw.jpg"
+    document.getElementById("carimg").style=("width:50vw;height:60vh;")
+    document.getElementById("carlogo").style=("width:10vw;height:20vh;")
+    document.getElementById("alternamecompany").innerHTML=("Bayerische Motoren Werke AG")
+    document.getElementById("alterfoundername").innerHTML=("Camillo Castiglioni,Franz Popp,Karl Rapp")
+    document.getElementById("alterfounded").innerHTML=("7 March 1916; 107 years ago")
+    document.getElementById("alterhead").innerHTML=("Munich, Germany")
+    document.getElementById("alterproduct").innerHTML=("2382305 cars")
+    document.getElementById("alterowner").innerHTML=("Stefan Quandt,Susanne Klatten")
+    document.getElementById("link").href="https://www.bmw.com/en/index.html"
+
+}
+function audi(){
+    document.getElementById("carname").innerHTML=("audi")
+    document.getElementById("Lamborghini-but").style=("background-color:royalblue")
+    document.getElementById("Bugatti-but").style=("background-color:royalblue")
+    document.getElementById("Ferrari-but").style=("background-color:royalblue")
+    document.getElementById("Mini-but").style=("background-color:royalblue")
+    document.getElementById("Porsche-but").style=("background-color:royalblue")
+    document.getElementById("Aston-but").style=("background-color:royalblue")
+    document.getElementById("Mercedes-but").style=("background-color:royalblue")
+    document.getElementById("McLaren-but").style=("background-color:royalblue")
+    document.getElementById("BMW-but").style=("background-color:royalblue")
+    document.getElementById("audi-but").style=("background-color:tomato")
+    document.getElementById("carlogo").src="audi-logo-removebg-preview.png"
+    document.getElementById("carimg").src="audi.jpg"
+    document.getElementById("carimg").style=("width:50vw;height:60vh;")
+    document.getElementById("carlogo").style=("width:15vw;height:20vh;")
+    document.getElementById("alternamecompany").innerHTML=("Audi AG")
+    document.getElementById("alterfoundername").innerHTML=("August Horch")
+    document.getElementById("alterfounded").innerHTML=("16 July 1909 in Zwickau")
+    document.getElementById("alterhead").innerHTML=("Ingolstadt, Germany")
+    document.getElementById("alterproduct").innerHTML=("1,802,073 units")
+    document.getElementById("alterowner").innerHTML=("The Volkswagen Group")
+    document.getElementById("link").href="https://www.audi.com/en.html"
+}
+function help(){
+    document.getElementById("topnav").style=("display:none;")
+    document.getElementById("centercontent").style=("display:none;")
+    document.getElementById("help").style=("display:flex;cursor: pointer;")
+    document.getElementById("close").style=("cursor: pointer;")
+    document.getElementById("foot").style=("display:none;")
+}
+function c(){
+    document.getElementById("topnav").style=("display: flex;")
+    document.getElementById("centercontent").style=("display: flex;")
+    document.getElementById("help").style=("display:none;")
+    document.getElementById("foot").style=("display:flex;")
+}
